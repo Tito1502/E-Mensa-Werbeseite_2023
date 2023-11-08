9b1b6530e57a3762df8a86e08550161b1c135a4f
Tito1502 <Tito1502@protonmail.com>
2023-11-08 19:55:56 +0100

    update

--- werbeseite/werbeseite.php
+++ werbeseite/werbeseite.php
@@ -47,7 +47,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <body>
 <nav>
     <div class="grid-container" id="mytopnav">
-        <div class="grid-item"><a href="#logogroß"><img src="" alt="Logo"></a></div>
+        <div class="grid-item logo"><a href="#logogroß"><img src="IMG/logo-studentenwerk.png" alt="Logo"></a></div>
         <div class="grid-item"><a href="#ankündigung">Ankündigung</a></div>
         <div class="grid-item"><a href="#speisen">Speisen</a></div>
         <div class="grid-item"><a href="#zahlen">Zahlen</a></div>
