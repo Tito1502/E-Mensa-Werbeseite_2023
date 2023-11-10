0cc2c6d1f86c64b292a09aa3492876ebb1c30205
VVR4ITH <deus_ex_machina051@mail.ru>
2023-11-10 16:47:17 +0100

    Aufgabe 5

--- beispiele/m2_5a_standardparameter.php
+++ beispiele/m2_5a_standardparameter.php
@@ -1,3 +1,11 @@
-<?php function add($a,$b=0){
+<?php
+
+/**
+ * Praktikum DBWT. Autoren:
+ * Bol, Daudov, 3539110
+ * Vorname2, Nachname2, Matrikelnummer2
+ *  Vorname2, Nachname2, Matrikelnummer2
+ */
+function add($a,$b=0){
     return $a+$b;
 }?>
--- beispiele/m2_5c_addform.php
+++ beispiele/m2_5c_addform.php
@@ -1,3 +1,11 @@
+
+/**
+* Praktikum DBWT. Autoren:
+* Bol, Daudov, 3539110
+* Vorname2, Nachname2, Matrikelnummer2
+*  Vorname2, Nachname2, Matrikelnummer2
+*/
+
 <!DOCTYPE html>
 <html>
 <head>
