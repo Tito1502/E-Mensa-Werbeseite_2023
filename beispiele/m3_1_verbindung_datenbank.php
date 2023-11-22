8f4fb3792658d9a59dd85db11ec6d4e8dc932efa
Philip <philip.engels@aliumni.fh-aachen.de>
2023-11-22 16:40:18 +0100

    Aufgaben 1-3 und 7

---
+++ beispiele/m3_1_verbindung_datenbank.php
@@ -0,0 +1,42 @@
+
+<!--
+- Praktikum E-Mensa Werbeseite. Autoren:
+- Jeremy, Mainka, 3567706
+- Philip, Engels, 3569528
+- Bol, Daudov, 3539110
+-->
+
+<?php
+
+$link = mysqli_connect
+(
+    "149.201.88.26", // Host der Datenbank
+    "dbwt", // Benutzername zur Anmeldung
+    "dbwt", // Passwort zur Anmeldung
+    "dbwt" // Auswahl der Datenbank
+);
+
+if (!$link)
+{
+    echo "Verbindung fehlgeschlagen: ", mysqli_connect_error();
+    exit();
+}
+
+$sql = "SELECT * FROM mitarbeiter";
+
+$result = mysqli_query($link, $sql);
+
+if (!$result)
+{
+    echo "Fehler während der Abfrage:  ", mysqli_error($link);
+    exit();
+}
+
+while ($row = mysqli_fetch_assoc($result)) {
+    echo $row['mid'], ' ', $row['vorname'], ' ', $row['nachname'], ',', ' ';
+}
+
+mysqli_free_result($result);
+mysqli_close($link);
+
+?>
--- beispiele/meal.php
+++ beispiele/meal.php
@@ -1,3 +1,10 @@
+<!--
+- Praktikum E-Mensa Werbeseite. Autoren:
+- Jeremy, Mainka, 3567706
+- Philip, Engels, 3569528
+- Bol, Daudov, 3539110
+-->
+
 <?php
 const GET_PARAM_MIN_STARS = 'search_min_stars';
 const GET_PARAM_SEARCH_TEXT = 'search_text';
@@ -18,6 +25,7 @@ $meal = [
     'description' => 'Die Süßkartoffeln werden vorsichtig aufgeschnitten und der Frischkäse eingefüllt.',
     'price_intern' => 2.90,
     'price_extern' => 3.90,
+    //verschachteltes array um mehrere Werte zu speichern
     'allergens' => $allergensmeal = array(11, 13),
     'amount' => 42             // Number of available meals
 ];
@@ -45,6 +53,7 @@ $sprache = $_GET['sprache'] ?? 'de';
 
 $art_bewertung = $_GET['art_bewertung'] ?? 'alle';
 
+//array zur Übersetzung der statischen texte
 $translation = [
     'Gericht' => 'Meal',
     'Allergene' => 'Allergens',
@@ -85,24 +94,29 @@ if (!empty($_GET[GET_PARAM_SEARCH_TEXT]))
 
 function calcMeanStars(array $ratings): float
 {
+    //$sum = 1 ist logisch falsch
     $sum = 0;
     foreach ($ratings as $rating)
     {
         $sum += $rating['stars'];
     }
-    if (count($ratings) > 0) {
+    if (count($ratings) > 0)
+    {
         return $sum / count($ratings);
-    } else {
+    } else
+    {
         return 0; // verhindert eine Division durch Null, wenn das Array leer ist
     }
 }
 
+//ermöglicht array elemente nach Anzahl Sterne zu sortieren
 function compareByStars($a, $b)
 {
     return $a['stars'] - $b['stars'];
 }
 
 ?>
+
 <!DOCTYPE html>
 <html lang="de">
 <head>
@@ -120,42 +134,49 @@ function compareByStars($a, $b)
 </head>
 <body>
 
-<a href="meal.php?sprache=de&art_bewertung=alle">Deutsch</a>
-<a href="meal.php?sprache=en&art_bewertung=alle">English</a>
+<!--
+ - Links zum ändern von Website Parametern
+ - Hinweis: Links anklicken oder nach Bewertungstexten suchen
+ - setzt die Seite auf Deutsch, da ich keine Lösung für das
+ - Problem gefunden habe
+ -->
+<a href="meal.php?sprache=de">Deutsch</a>
+<a href="meal.php?sprache=en">English</a>
 <br>
 <a href="meal.php?art_bewertung=alle">ALLE</a>
 <a href="meal.php?art_bewertung=TOP">TOP</a>
 <a href="meal.php?art_bewertung=FLOPP">FLOPP</a>
 
-
-
 <?php
-if($sprache == 'en')
+//Zeigt das Gericht der Sprache entsprechend an
+if ($sprache == 'en')
 {
     echo "<h1>{$translation['Gericht']}: {$meal['name']} </h1>";
-}elseif($sprache == 'de')
+} elseif ($sprache == 'de')
 {
     echo "<h1>Gericht: {$meal['name']}</h1>";
 }
 ?>
 
-
 <?php
-if(isset($_GET["show_description"]))
+//zeigt die Beschreibung an, sofern show_description = 1
+//muss manuell in die suchleiste getippt werden : "&show_description=0" (default: Beschreibung aktiv)
+if (isset($_GET["show_description"]))
 {
     $show_description = $_GET["show_description"];
 }
-if($show_description)
+if ($show_description)
 {
     echo "<p> {$meal['description']} </p>";
 }
 ?>
 
 <?php
-if($sprache == 'en')
+//Zeigt die Überschrift "Allergene" der Sprache entsprechend an
+if ($sprache == 'en')
 {
     echo "{$translation['Allergene']}:";
-}elseif($sprache == 'de')
+} elseif ($sprache == 'de')
 {
     echo "Allergene:";
 }
@@ -163,7 +184,8 @@ if($sprache == 'en')
 
 <ul>
     <?php
-    for($i = 0; $i < count($allergensmeal); $i++)
+    //gibt die Allergene als ungeordnete Liste zurück
+    for ($i = 0; $i < count($allergensmeal); $i++)
     {
         echo "<li>{$allergens[$allergensmeal[$i]]}</li>";
     }
@@ -171,10 +193,11 @@ if($sprache == 'en')
 </ul>
 
 <?php
-if($sprache == 'en')
+//Trennt den String price_extern auf und wandelt ihn in den richtigen Syntax um
+if ($sprache == 'en')
 {
     echo "{$translation['Preis']}:";
-}else if($sprache == 'de')
+} else if ($sprache == 'de')
 {
     echo "Preis: <br>";
 }
@@ -184,7 +207,8 @@ echo "<ul><li>{$preis}€</li></ul>"
 ?>
 
 <?php
-if(isset($_POST['submit']))
+//schickt den Suchtext mit Post an den Server
+if (isset($_POST['submit']))
 {
     $searchtext = $_POST['search_text'];
 }
@@ -192,30 +216,40 @@ if(isset($_POST['submit']))
 
 
 <?php
-if($sprache == 'en')
+//zeigt die Überschrift "Bewertungen" bzw "Ratings" mitsamt durchschnittlicher Sternzahl an
+if ($sprache == 'en')
 {
     echo "<h1> {$translation['Bewertungen']} ({$translation['Insgesamt']}:" . calcMeanStars($ratings) . ")</h1>";
-}elseif($sprache == 'de')
+} elseif ($sprache == 'de')
 {
     echo "<h1>Bewertungen (Insgesamt:" . calcMeanStars($ratings) . ")</h1>";
 }
 ?>
 
+<!--
+ - Textfeld für Suchtext, der den eingegebenen Text weiterhin anzeigt
+ -->
 <form method="get">
     <label for="search_text">Filter:</label>
-    <input id="search_text" type="text" name="search_text" value="<?php echo isset($_GET['search_text']) ? htmlspecialchars($_GET['search_text']) : ''; ?>">
-    <input type="submit" value="<?php if($sprache == 'en'){echo $translation['Suchen'];}elseif ($sprache == 'de'){echo "Suchen";} ?>">
+    <input id="search_text" type="text" name="search_text"
+           value="<?php echo isset($_GET['search_text']) ? htmlspecialchars($_GET['search_text']) : ''; ?>">
+    <input type="submit" value="<?php if ($sprache == 'en') {
+        echo $translation['Suchen'];
+    } elseif ($sprache == 'de') {
+        echo "Suchen";
+    } ?>">
 </form>
 <table class="rating">
     <thead>
     <tr>
         <?php
-        if($sprache == 'en')
+        //zeigt die Tabellen überschriften in der entsprechenden Sprache an
+        if ($sprache == 'en')
         {
             echo "<td>Text</td>
 		              <td>{$translation['Autor']}</td>
                       <td>{$translation['Sterne']}</td>";
-        }elseif ($sprache == 'de')
+        } elseif ($sprache == 'de')
         {
             echo "<td>Text</td>
 		              <td>Autor</td>
@@ -226,28 +260,28 @@ if($sprache == 'en')
     </thead>
     <tbody>
     <?php
-    if($_GET['art_bewertung'] == 'alle' ?? 'alle')
+    //Wenn art_bewertung nicht gesetzt ist oder weder "TOP","FLOPP" noch "alle" ist, wird es auf alle gesetzt
+    if(!isset($_GET['art_bewertung']) || ($_GET['art_bewertung'] != 'FLOPP' && $_GET['art_bewertung'] != 'TOP' && $_GET['art_bewertung'] != 'alle'))
+    {
+        $art_bewertung = 'alle';
+    }
+    //sortiert das Array nach Anzahl Sterne
+    usort($showRatings, 'compareByStars');
+    //nimmt die niedrigste Anzahl Sterne aus dem Array
+    if ($art_bewertung == 'FLOPP')
+    {
+        $top_star = $showRatings[0]['stars'];
+        //nimmt die höchste Anzahl Sterne aus dem Array
+    } else if ($art_bewertung == 'TOP')
+    {
+        $top_star = $showRatings[count($showRatings) - 1]['stars'];
+    }
+    //gibt alle "TOP" bzw "FLOPP" Bewertungen aus
+    if ($art_bewertung == 'FLOPP' || $art_bewertung == 'TOP')
     {
         foreach ($showRatings as $rating)
         {
-            echo "<tr>
-                <td class='rating_text'>{$rating['text']}</td>
-                <td class='rating_author'>{$rating['author']}</td>
-                <td class='rating_stars'>{$rating['stars']}</td>
-              </tr>";
-        }
-    }else{
-        usort($showRatings, 'compareByStars');
-        if($_GET['art_bewertung'] == 'FLOPP' ?? 'alle')
-        {
-            $top_star = $showRatings[0]['stars'];
-        }elseif ($_GET['art_bewertung'] == 'TOP' ?? 'alle')
-        {
-            $top_star = $showRatings[3]['stars'];
-        }
-        foreach ($showRatings as $rating)
-        {
-            if($rating['stars'] == $top_star)
+            if ($rating['stars'] == $top_star)
             {
                 echo "<tr>
                 <td class='rating_text'>{$rating['text']}</td>
@@ -255,6 +289,16 @@ if($sprache == 'en')
                 <td class='rating_stars'>{$rating['stars']}</td>
               </tr>";
             }
+        }
+        //gibt alle Bewertungen aus
+    } else{
+        foreach ($showRatings as $rating)
+        {
+            echo "<tr>
+                <td class='rating_text'>{$rating['text']}</td>
+                <td class='rating_author'>{$rating['author']}</td>
+                <td class='rating_stars'>{$rating['stars']}</td>
+              </tr>";
         }
     }
     ?>
